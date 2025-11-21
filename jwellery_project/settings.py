b4753039fff131cfbfcfcf08eeaf8ac18cee0cf7
Nanthini Palani <48705904+Nanthini7701@users.noreply.github.com>
2025-11-21 21:10:58 +0530

    Add new host to ALLOWED_HOSTS in settings.py

--- jwellery_project/settings.py
+++ jwellery_project/settings.py
@@ -6,7 +6,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY','change-me')
 DEBUG = os.getenv('DEBUG','1') == '1'
-ALLOWED_HOSTS = ['NANTHINIHARI.pythonanywhere.com','127.0.0.1','*']
+ALLOWED_HOSTS = ['NANTHINIHARI.pythonanywhere.com','127.0.0.1','*','https://jwellexue.onrender.com']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
